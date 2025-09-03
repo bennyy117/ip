b2f7101bfb3c955f19de332b377397b6a1b3d729
bennyy117 <naryloan@gmail.com>
2025-09-03 16:23:32 +0800

    Level-1

--- src/main/java/Nary.java
+++ src/main/java/Nary.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Nary {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        printLine();
         System.out.println(" Hello! I'm Nary");
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
+        printLine();
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine().trim();
+            if (input.equals("bye")) {
+                printLine();
+                System.out.println(" Bye. Hope to see you again soon!");
+                printLine();
+                break;
+            } else {
+                printLine();
+                System.out.println(" " + input);
+                printLine();
+            }
+        }
+        sc.close();
+    }
+
+    private static void printLine() {
         System.out.println("____________________________________________________________");
     }
 }
