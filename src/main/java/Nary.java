774588e6d4d0add2fef56e716fda29a8cada1f2c
bennyy117 <naryloan@gmail.com>
2025-09-03 16:25:23 +0800

    Level-3

--- src/main/java/Nary.java
+++ src/main/java/Nary.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Nary {
+    private static ArrayList<String> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         printLine();
         System.out.println(" Hello! I'm Nary");
@@ -10,17 +13,31 @@ public class Nary {
         Scanner sc = new Scanner(System.in);
         while (true) {
             String input = sc.nextLine().trim();
+
             if (input.equals("bye")) {
                 printLine();
                 System.out.println(" Bye. Hope to see you again soon!");
                 printLine();
                 break;
-            } else {
+            } else if (input.equals("list")) {
                 printLine();
-                System.out.println(" " + input);
+                if (tasks.isEmpty()) {
+                    System.out.println(" No tasks yet!");
+                } else {
+                    System.out.println(" Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                    }
+                }
+                printLine();
+            } else { // add task
+                tasks.add(input);
+                printLine();
+                System.out.println(" added: " + input);
                 printLine();
             }
         }
+
         sc.close();
     }
 
