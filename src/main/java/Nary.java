b4160d260b3dbda3ce61957b00d175940b9b7bcc
bennyy117 <naryloan@gmail.com>
2025-09-03 17:05:11 +0800

    Level-3

--- src/main/java/Nary.java
+++ src/main/java/Nary.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Nary {
-    private static ArrayList<String> tasks = new ArrayList<>();
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         printLine();
@@ -20,18 +20,24 @@ public class Nary {
                 printLine();
                 break;
             } else if (input.equals("list")) {
+                printTasks();
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks.get(index).markAsDone();
                 printLine();
-                if (tasks.isEmpty()) {
-                    System.out.println(" No tasks yet!");
-                } else {
-                    System.out.println(" Here are the tasks in your list:");
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println(" " + (i + 1) + ". " + tasks.get(i));
-                    }
-                }
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks.get(index));
                 printLine();
-            } else { // add task
-                tasks.add(input);
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks.get(index).markAsNotDone();
+                printLine();
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks.get(index));
+                printLine();
+            } else { // new task
+                Task t = new Task(input);
+                tasks.add(t);
                 printLine();
                 System.out.println(" added: " + input);
                 printLine();
@@ -41,6 +47,18 @@ public class Nary {
         sc.close();
     }
 
+    private static void printTasks() {
+        printLine();
+        if (tasks.isEmpty()) {
+            System.out.println(" No tasks yet!");
+        } else {
+            System.out.println(" Here are the tasks in your list:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println(" " + (i + 1) + "." + tasks.get(i));
+            }
+        }
+        printLine();
+    }
 
     private static void printLine() {
         System.out.println("____________________________________________________________");
